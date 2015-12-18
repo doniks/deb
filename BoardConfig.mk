e711d87a0bcbb235cb5a36e73d081e4739b7278e
Chih-Hung Hsieh <chh@google.com>
2015-12-18 11:20:36 -0800

    Move deb to clang by default.
    BUG: 23163853
    Change-Id: Ib2f2e6baee5de5358348bccd9a97e6a9b81c9a16

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,5 +24,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/asus/deb
 
 TARGET_RECOVERY_FSTAB = device/asus/deb/fstab.deb
 
+USE_CLANG_PLATFORM_BUILD := true
+
 -include vendor/asus/deb/BoardConfigVendor.mk
 include device/asus/flo/BoardConfigCommon.mk
